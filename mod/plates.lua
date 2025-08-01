52e5defb4ab23513314f2729fdd1f41823f92a58
singletail <t@wse.nyc>
2025-08-01 14:10:09 -0400

    housekeeping

--- mod/plates.lua
+++ mod/plates.lua
@@ -78,39 +78,39 @@ local function cp(t, vals)
 end
 
 local theme = {
-    default = { w=0, h=0, x=0, y=0, a=a.tl, pa=a.tl, parent='base', alpha=1, scale=0.4, f='Prompt-Regular', fs=14, strata='BACKGROUND', layer='ARTWORK', level=0, fh=a.l, fv=a.t },
+    default = { w = 0, h = 0, x = 0, y = 0, a = a.tl, pa = a.tl, parent = 'base', alpha = 1, scale = 0.4, f = 'Prompt-Regular', fs = 14, strata = 'BACKGROUND', layer = 'ARTWORK', level = 0, fh = a.l, fv = a.t },
     path = {
         tx = [[Interface\AddOns\ctrl\assets\nameplate\]],
         font = [[Interface\AddOns\ctrl\assets\fnt\]],
     },
     f = {
-        ['base'] = { w=320, h=48, a=a.c, pa=a.c, level=1 },
-        ['health'] = { w=294, h=44, x=24, y=-2, level=2 },
-        ['cast'] = { w=294, h=16, x=24, y=0, a=a.tl, pa=a.bl, level=4 },
-        ['castbk'] = { w=294, h=16, x=0, y=0, a=a.tl, pa=a.tl, level=3, parent='cast' },
-        ['top'] = { w=320, h=48, level=10 },
+        ['base'] = { w = 320, h = 48, a = a.c, pa = a.c, level = 1 },
+        ['health'] = { w = 294, h = 44, x = 24, y = -2, level = 2 },
+        ['cast'] = { w = 294, h = 16, x = 24, y = 0, a = a.tl, pa = a.bl, level = 4 },
+        ['castbk'] = { w = 294, h = 16, x = 0, y = 0, a = a.tl, pa = a.tl, level = 3, parent = 'cast' },
+        ['top'] = { w = 320, h = 48, level = 10 },
     },
     tx = {
-        ['bk'] = { t='320_bk.png', level=-7, alpha=0.6 },
-        ['cap'] = { t='320_cap_flat.png', h=48, w=24, level=-6, alpha=0.5 },
-        ['health'] = { t='320_hbar_flat.png', parent='health', level=-6, alpha=0.5 },
-        ['castbk'] = { t='320_hbar_flat.png', parent='castbk', level=-6, alpha=0.5 },
-        ['cast'] = { t='320_hbar_flat.png', parent='cast', level=-5, alpha=0.5 },
-        ['shadow'] = { t='320_shadow.png', parent='top', w=334, h=60, a=a.c, pa=a.c, level=-5, alpha=0.6 },
-        ['frame'] = { t='320_frame.png', parent='top', level=-4, alpha=0.5 },
-        ['glow'] = { t='320_glow.png', parent='top', w=334, h=60, a=a.c, pa=a.c, level=-2, alpha=0.1 },
-        ['box'] = { t='box.png', parent='top', w=64, h=64, a=a.c, pa=a.c, level=-2, alpha=1 },
+        ['bk'] = { t = '320_bk.png', level = -7, alpha = 0.6 },
+        ['cap'] = { t = '320_cap_flat.png', h = 48, w = 24, level = -6, alpha = 0.5 },
+        ['health'] = { t = '320_hbar_flat.png', parent = 'health', level = -6, alpha = 0.5 },
+        ['castbk'] = { t = '320_hbar_flat.png', parent = 'castbk', level = -6, alpha = 0.5 },
+        ['cast'] = { t = '320_hbar_flat.png', parent = 'cast', level = -5, alpha = 0.5 },
+        ['shadow'] = { t = '320_shadow.png', parent = 'top', w = 334, h = 60, a = a.c, pa = a.c, level = -5, alpha = 0.6 },
+        ['frame'] = { t = '320_frame.png', parent = 'top', level = -4, alpha = 0.5 },
+        ['glow'] = { t = '320_glow.png', parent = 'top', w = 334, h = 60, a = a.c, pa = a.c, level = -2, alpha = 0.1 },
+        ['box'] = { t = 'box.png', parent = 'top', w = 64, h = 64, a = a.c, pa = a.c, level = -2, alpha = 1 },
     },
     fs = {
-        [1] = { fs=26, parent='top', x=0, y=0, w=48, h=48, fh=a.c, fv=a.m },
-        [2] = { f='Prompt-Medium', fs=16, parent='top', w=240, h=14, x=42, y=-2 },
-        [3] = { parent='top', w=200, h=14, x=42, y=-16 },
-        [4] = { parent='top', w=120, h=14, x=42, y=-30 },
-        [5] = { parent='top', h=14, w=52, x=-4, y=-2, a=a.tr, pa=a.tr, fh=a.r },
-        [6] = { parent='top', h=14, w=80, x=-4, y=-16, a=a.tr, pa=a.tr, fh=a.r },
-        [7] = { parent='top', h=14, w=160, x=-4, y=-30, a=a.tr, pa=a.tr, fh=a.r },
-        [8] = { parent='cast', h=14, w=160, x=42, y=0, a=a.tl, pa=a.tl, fh=a.l }, --cast
-        [9] = { f='SourceCodePro-Medium', fs=16, parent='top', x=42, y=-48, a=a.tl, pa=a.tl },
+        [1] = { fs = 26, parent = 'top', x = 0, y = 0, w = 48, h = 48, fh = a.c, fv = a.m },
+        [2] = { f = 'Prompt-Medium', fs = 16, parent = 'top', w = 240, h = 14, x = 42, y = -2 },
+        [3] = { parent = 'top', w = 200, h = 14, x = 42, y = -16 },
+        [4] = { parent = 'top', w = 120, h = 14, x = 42, y = -30 },
+        [5] = { parent = 'top', h = 14, w = 52, x = -4, y = -2, a = a.tr, pa = a.tr, fh = a.r },
+        [6] = { parent = 'top', h = 14, w = 80, x = -4, y = -16, a = a.tr, pa = a.tr, fh = a.r },
+        [7] = { parent = 'top', h = 14, w = 160, x = -4, y = -30, a = a.tr, pa = a.tr, fh = a.r },
+        [8] = { parent = 'cast', h = 14, w = 160, x = 42, y = 0, a = a.tl, pa = a.tl, fh = a.l }, --cast
+        [9] = { f = 'SourceCodePro-Medium', fs = 16, parent = 'top', x = 42, y = -48, a = a.tl, pa = a.tl },
     },
 }
 
@@ -249,16 +249,18 @@ end
 -- Spellcast Event Functions
 
 local function OnUpdateSpell(self)
-    local name, displayName, textureID, startTimeMs, endTimeMs, _, castID, notInterruptible, spellID = UnitCastingInfo(self.unit)
-    if not name then name, displayName, textureID, startTimeMs, endTimeMs, _, notInterruptible, spellID = UnitChannelInfo(self.unit) end
+    local name, displayName, textureID, startTimeMs, endTimeMs, _, castID, notInterruptible, spellID = UnitCastingInfo(
+    self.unit)
+    if not name then name, displayName, textureID, startTimeMs, endTimeMs, _, notInterruptible, spellID = UnitChannelInfo(
+        self.unit) end
     if not name then return end
-    self.fs[8]:SetText(tostring(name)..' '..tostring(spellID))
+    self.fs[8]:SetText(tostring(name) .. ' ' .. tostring(spellID))
     if notInterruptible then self.tx.cast:SetVertexColor(0, 0, 1, 0.5) else self.tx.cast:SetVertexColor(0.5, 0.5, 0, 1) end
     startTimeMs = startTimeMs or 0
     endTimeMs = endTimeMs or 1
     local time = ((GetTime() * 1000) - startTimeMs)
     local endtime = (endTimeMs - startTimeMs)
-    local barWidth = theme.f.cast.w * (time/endtime)
+    local barWidth = theme.f.cast.w * (time / endtime)
     self.f.cast:SetWidth(barWidth)
 end
 
@@ -279,9 +281,13 @@ local function UpdateHealth(self)
     local hm = UnitHealthMax(self.unit) or 1
     local hp = math.floor((h / hm) * 100)
     local hmod, hstr = 1, ''
-    if hm>1000000 then hmod=1000000;hstr='m' elseif hm>1000 then hmod=1000;hstr='k' end
+    if hm > 1000000 then
+        hmod = 1000000; hstr = 'm'
+    elseif hm > 1000 then
+        hmod = 1000; hstr = 'k'
+    end
     self.fs[5]:SetText(string.format('%d%%', hp))
-    self.fs[6]:SetText(string.format('%.1f/%.1f%s', h/hmod, hm/hmod, hstr))
+    self.fs[6]:SetText(string.format('%.1f/%.1f%s', h / hmod, hm / hmod, hstr))
     self.f.health:SetWidth(hp * (theme.f.health.w / 100))
     self:UpdateTarget()
 end
@@ -290,8 +296,8 @@ end
 
 local function UpdateTarget(self)
     -- Target Name
-    local target = self.unit..'target'
-    if not UnitExists(target) then 
+    local target = self.unit .. 'target'
+    if not UnitExists(target) then
         ctrl.plates:debug('UpdateTarget', 'No target found', self.unit)
         self.fs[7]:SetText('')
         return
@@ -310,11 +316,11 @@ end
 
 local function UpdateThreat(self)
     local col = {
-        off = {1, 1, 1, 0},
-        dim = {1, 1, 1, 0.2},
-        orange = {1, 0.5, 0, 1},
-        yellow = {1, 1, 0, 1},
-        red = {1, 0, 0, 1},
+        off = { 1, 1, 1, 0 },
+        dim = { 1, 1, 1, 0.2 },
+        orange = { 1, 0.5, 0, 1 },
+        yellow = { 1, 1, 0, 1 },
+        red = { 1, 0, 0, 1 },
     }
     local col_frame = 'dim'
     local col_glow = 'off'
@@ -325,8 +331,9 @@ local function UpdateThreat(self)
         if IsInRaid() or IsInGroup() then
             local role = UnitGroupRolesAssigned('player')
             local threatStatus = UnitThreatSituation('player', self.unit)
+        end
     end
---[[
+    --[[
     if UnitAffectingCombat(self.unit) then
         self.tx.frame:SetVertexColor(c.rgba.r)
         if IsInRaid() or IsInGroup() then
@@ -371,7 +378,7 @@ local function Draw(self)
     self.fs[1]:SetText(self.str.icon or s.question)
     self.fs[2]:SetText(self.color.hex .. (self.displayName or self.name or s.question))
     self.fs[3]:SetText(self.str.t1 or self.player.guildName or '')
-    self.fs[4]:SetText(self.str.t2 or self.player.guildRank or'')
+    self.fs[4]:SetText(self.str.t2 or self.player.guildRank or '')
     self.tx.health:SetVertexColor(self.color.rgba[1], self.color.rgba[2], self.color.rgba[3], self.color.rgba[4])
     self.tx.cap:SetVertexColor(self.color.rgba[1], self.color.rgba[2], self.color.rgba[3], self.color.rgba[4])
     self.tx.frame:SetVertexColor(self.color.frame[1], self.color.frame[2], self.color.frame[3], self.color.frame[4])
@@ -388,7 +395,8 @@ local function ConfigPlayer(self)
     cp(self.color.rgba, c.class[self.player.class])
     self.player.isFriend = C_FriendList.IsFriend(self.guid)
     self.str.icon = self.player.isFriend and s.heart or s[self.guildName] or s[self.player.class] or s.crit
-    if self.player.isFriend then self.color.frame=c.rgba.p elseif s[self.player.guildName] then self.color.frame=c.rgba.c end
+    if self.player.isFriend then self.color.frame = c.rgba.p elseif s[self.player.guildName] then self.color.frame = c
+        .rgba.c end
 end
 
 local function CheckDB(self)
@@ -405,7 +413,8 @@ local function ConfigNPC(self)
     local _, _, _, _, _, npcId, spawnId = strsplit("-", self.guid)
     self.npc.npcId = tonumber(npcId)
     self.npc.spawnIndex = bit.rshift(bit.band(tonumber(string.sub(spawnId, 1, 5), 16), 0xffff8), 3)
-    if self.npc.spawnIndex and tonumber(self.npc.spawnIndex)>0 then self.displayName=self.name..' '..tostring(self.npc.spawnIndex) end
+    if self.npc.spawnIndex and tonumber(self.npc.spawnIndex) > 0 then self.displayName = self.name ..
+        ' ' .. tostring(self.npc.spawnIndex) end
     self.npc.reaction = UnitReaction(self.unit, 'player')
     self.npc.classification = UnitClassification(self.unit)
     self.npc.creatureType = UnitCreatureType(self.unit)
@@ -419,7 +428,8 @@ local function ConfigNPC(self)
     end
     self.str.t1 = self.npc.creatureType or ''
     if self.npc.creatureFamily and self.npc.creatureFamily ~= self.npc.creatureType then
-        self.str.t1 = self.str.t1 .. ' - ' .. self.npc.creatureFamily end
+        self.str.t1 = self.str.t1 .. ' - ' .. self.npc.creatureFamily
+    end
     self.str.t2 = tostring(self.npc.npcId)
     self:CheckDB()
 end
@@ -455,7 +465,9 @@ local function Reset(self)
     self:Hide()
 end
 
-local function Hide(self) self.f.base:Hide(); self.f.cast:Hide() end
+local function Hide(self)
+    self.f.base:Hide(); self.f.cast:Hide()
+end
 local function ClearAllPoints(self) self.f.base:ClearAllPoints() end
 local function SetParent(self, parent) self.f.base:SetParent(parent) end
 local function SetPoint(self, anc, parent, panc, x, y) self.f.base:SetPoint(anc, parent, panc, x, y) end
@@ -493,7 +505,7 @@ function ctrl.plates:assign(np, nameplate)
 end
 
 function ctrl.plates:attach(nameplate)
-    for i=1,40 do
+    for i = 1, 40 do
         if not ctrl.np[i].nameplate then
             self:assign(ctrl.np[i], nameplate)
             return
@@ -507,7 +519,7 @@ function ctrl.plates:opt(v)
     local opt = {}
     cp(opt, theme.default)
     cp(opt, v)
-    if not opt.anchors then opt.anchors = {{ a=opt.a, pa=opt.pa, x=opt.x, y=opt.y }} end
+    if not opt.anchors then opt.anchors = { { a = opt.a, pa = opt.pa, x = opt.x, y = opt.y } } end
     return opt
 end
 
@@ -527,10 +539,13 @@ function ctrl.plates:txAdd(np, k, v)
     local opt = self:opt(v)
     if type(opt.parent) == 'string' then opt.parent = np.f[opt.parent] or np.f.base or UIParent end
     np.tx[k] = opt.parent:CreateTexture(nil, opt.layer, nil, opt.level)
-    np.tx[k]:SetTexture(theme.path.tx..opt.t, a.w.c, a.w.c, 'TRILINEAR')
+    np.tx[k]:SetTexture(theme.path.tx .. opt.t, a.w.c, a.w.c, 'TRILINEAR')
     np.tx[k]:SetParent(opt.parent)
-    if opt.w>0 and opt.h>0 then np.tx[k]:SetSize(opt.w, opt.h); self:anchor(np.tx[k], opt) else
-    np.tx[k]:SetSize(opt.parent:GetWidth(), opt.parent:GetHeight()); np.tx[k]:SetAllPoints(opt.parent) end
+    if opt.w > 0 and opt.h > 0 then
+        np.tx[k]:SetSize(opt.w, opt.h); self:anchor(np.tx[k], opt)
+    else
+        np.tx[k]:SetSize(opt.parent:GetWidth(), opt.parent:GetHeight()); np.tx[k]:SetAllPoints(opt.parent)
+    end
     np.tx[k]:SetAlpha(opt.alpha or 1)
 end
 
@@ -546,14 +561,15 @@ function ctrl.plates:fsAdd(np, k, v)
 end
 
 function ctrl.plates:anchor(e, opt)
-    for n = 1, #opt.anchors do e:SetPoint(opt.anchors[n].a, opt.parent, opt.anchors[n].pa, opt.anchors[n].x, opt.anchors[n].y) end
+    for n = 1, #opt.anchors do e:SetPoint(opt.anchors[n].a, opt.parent, opt.anchors[n].pa, opt.anchors[n].x,
+            opt.anchors[n].y) end
 end
 
 function ctrl.plates:build(np)
     self:fAdd(np, 'base', theme.f.base)
-    for k,v in pairs(theme.f) do if k ~= 'base' then self:fAdd(np, k, v) end end
-    for k,v in pairs(theme.tx) do self:txAdd(np, k, v) end
-    for k,v in pairs(theme.fs) do self:fsAdd(np, k, v) end
+    for k, v in pairs(theme.f) do if k ~= 'base' then self:fAdd(np, k, v) end end
+    for k, v in pairs(theme.tx) do self:txAdd(np, k, v) end
+    for k, v in pairs(theme.fs) do self:fsAdd(np, k, v) end
     self:fn(np)
     np:Reset()
 end
@@ -561,7 +577,7 @@ end
 -- Setup & Generation
 
 function ctrl.plates:redraw()
-    for i=1,40 do
+    for i = 1, 40 do
         if ctrl.np[i].nameplate then ctrl.np[i].nameplate.UnitFrame:Hide() end
     end
 end
@@ -576,7 +592,7 @@ end
 
 function ctrl.plates:setup()
     ctrl.np = ctrl.np or {}
-    for i=1,40 do ctrl.np[i] = ctrl.np[i] or self:new(i) end
+    for i = 1, 40 do ctrl.np[i] = ctrl.np[i] or self:new(i) end
 end
 
 ctrl.plates:init()
